eaa489521dbe89b133c474c62219644978c2190a
Yosi.Zanko <yosi.zanko@codefresh.io>
2021-11-16 18:33:08 +0200

    structure change

--- index.ts
+++ index.ts
@@ -1,14 +1,9 @@
 import {QueryOptions} from "@google-cloud/bigquery";
+const {BigQuery} = require('@google-cloud/bigquery');
+const bigquery = new BigQuery();
 
-function main() {
-    // [START bigquery_query]
-    // [START bigquery_client_default_credentials]
-    // Import the Google Cloud client library using default credentials
-    const {BigQuery} = require('@google-cloud/bigquery');
-    const bigquery = new BigQuery();
-
-    const queryGlobalStats = (accountId: number, dateFrom: Date, dateTo:Date) =>
-        `SELECT DATETIME_TRUNC(DATETIME(\`platform_workflow\`.start_time, 'UTC'), 
+const queryGlobalStats = (accountId: number, dateFrom: Date, dateTo:Date) =>
+    `SELECT DATETIME_TRUNC(DATETIME(\`platform_workflow\`.start_time, 'UTC'), 
 DAY) \`platform_workflow__time_day\`, 
 count(distinct \`platform_workflow\`.workflow_id) \`platform_workflow__total_executions\`,
  avg(\`platform_workflow\`.duration) \`platform_workflow__average_duration\`,
@@ -24,8 +19,8 @@ FROM
        GROUP BY 1 ORDER BY 1 ASC LIMIT 100;`
 
 
-    const queryPipelineStats = (accountId: number, dateFrom: Date, dateTo:Date, pipelineId: number) =>
-        `SELECT DATETIME_TRUNC(DATETIME(\`platform_workflow\`.start_time, 'UTC'), 
+const queryPipelineStats = (accountId: number, dateFrom: Date, dateTo:Date, pipelineId: number) =>
+    `SELECT DATETIME_TRUNC(DATETIME(\`platform_workflow\`.start_time, 'UTC'), 
 DAY) \`platform_workflow__time_day\`, 
 count(distinct \`platform_workflow\`.workflow_id) \`platform_workflow__total_executions\`,
  avg(\`platform_workflow\`.duration) \`platform_workflow__average_duration\`,
@@ -41,8 +36,8 @@ FROM
        and (\`platform_workflow\`.pipeline_name = 'pipeline_${pipelineId}')
        GROUP BY 1 ORDER BY 1 ASC LIMIT 100;`
 
-    const queryTop10Stats = (accountId: number, dateFrom: Date, dateTo:Date) =>
-        `select platform_workflow.pipeline_name platform_workflow__pipeline_name,
+const queryTop10Stats = (accountId: number, dateFrom: Date, dateTo:Date) =>
+    `select platform_workflow.pipeline_name platform_workflow__pipeline_name,
                 platform_workflow.pipeline_namespace platform_workflow__pipeline_namespace,
                 platform_workflow.runtime platform_workflow__runtime,
                 count(distinct platform_workflow.workflow_id) platform_workflow__total_executions,
@@ -60,64 +55,67 @@ FROM
             group by	1,	2,	3
             order by	4 desc`
 
-    // [END bigquery_client_default_credentials]
-    async function query(account: number, dateFrom: Date, dateTo:Date, queryType: QueryTypeEnum, pipelineID?: number ) {
+enum QueryTypeEnum {
+    global=  'global analytics  ' ,
+    pipeline='pipeline analytics',
+    top10=   'top10             '
+}
+async function query(account: number, dateFrom: Date, dateTo:Date, queryType: QueryTypeEnum, pipelineID?: number ) {
     //async function query() {
-        // Queries the U.S. given names dataset for the state of Texas.
+    // Queries the U.S. given names dataset for the state of Texas.
 
-        let queryToRun;
+    let queryToRun;
 
-        if (queryType === QueryTypeEnum.global) {
-            queryToRun = queryGlobalStats(account, dateFrom, dateTo)
-        }
-        else if ((queryType === QueryTypeEnum.pipeline) && (pipelineID) ){
-            queryToRun = queryPipelineStats(account, dateFrom, dateTo, pipelineID)
-        }
-        else if (queryType === QueryTypeEnum.top10) {
-            queryToRun = queryTop10Stats(account,dateFrom,dateTo)
-        }
-
-        //console.log(queryToRun)
-        // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
-        const options = {
-            query: queryToRun,
-            // Location must match that of the dataset(s) referenced in the query.
-            location: 'US',
-        };
-        const execStartTime = new Date().getTime();
-
-        // Run the query as a job
-        const [job] = await bigquery.createQueryJob(options);
-        const jobCreationDuration = new Date().getTime() - execStartTime
-
-        // Wait for the query to finish
-        const [rows] = await job.getQueryResults();
-        const afterGetQueryResults = new Date().getTime() - execStartTime
-        let numOfRows = 0
-
-        rows.forEach((row: any) => {
-            numOfRows++;
-        });
-
-        const execDuration = new Date().getTime() - job.metadata.statistics.startTime
-        const execDuration1 = new Date().getTime() - job.metadata.statistics.creationTime
-        const execDuration2 = new Date().getTime() - execStartTime
-       // console.log(job.metadata)
-        console.log(`${queryType} - Account:${ (account<600)?"Small ":(account<900)?"Medium":"Large "} - ${account}, `+
-             `TotalTime:${execDuration2} ms, ` +
-            // sinceStatsStartTime:${execDuration} ms, sinceStatsCreationTime: ${execDuration1} ms, `+
-            `JobCreationTime:${jobCreationDuration} ms, QueryResultsTime:${afterGetQueryResults - jobCreationDuration} ms ` //+
-            //`Days:${(dateTo.getTime()-dateFrom.getTime())/1000 /24/60/60}, `+
-            //`NumOfRows:${numOfRows}`
-        )
+    if (queryType === QueryTypeEnum.global) {
+        queryToRun = queryGlobalStats(account, dateFrom, dateTo)
     }
-    enum QueryTypeEnum {
-        global=  'global analytics  ' ,
-        pipeline='pipeline analytics',
-        top10=   'top10             '
+    else if ((queryType === QueryTypeEnum.pipeline) && (pipelineID) ){
+        queryToRun = queryPipelineStats(account, dateFrom, dateTo, pipelineID)
     }
-    async function runQueries(): Promise<number>  {
-        const promises = [
+    else if (queryType === QueryTypeEnum.top10) {
+        queryToRun = queryTop10Stats(account,dateFrom,dateTo)
+    }
+
+    //console.log(queryToRun)
+    // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
+    const options = {
+        query: queryToRun,
+        // Location must match that of the dataset(s) referenced in the query.
+        location: 'US',
+    };
+    const execStartTime = new Date().getTime();
+
+    // Run the query as a job
+    const [job] = await bigquery.createQueryJob(options);
+    const jobCreationDuration = new Date().getTime() - execStartTime
+
+    // Wait for the query to finish
+    const [rows] = await job.getQueryResults();
+    const afterGetQueryResults = new Date().getTime() - execStartTime
+    let numOfRows = 0
+
+    rows.forEach((row: any) => {
+        numOfRows++;
+    });
+
+    const execDuration = new Date().getTime() - job.metadata.statistics.startTime
+    const execDuration1 = new Date().getTime() - job.metadata.statistics.creationTime
+    const execDuration2 = new Date().getTime() - execStartTime
+    // console.log(job.metadata)
+    console.log(`${queryType} - Account:${ (account<600)?"Small ":(account<900)?"Medium":"Large "} - ${account}, `+
+        `TotalTime:${execDuration2} ms, ` +
+        // sinceStatsStartTime:${execDuration} ms, sinceStatsCreationTime: ${execDuration1} ms, `+
+        `JobCreationTime:${jobCreationDuration} ms, QueryResultsTime:${afterGetQueryResults - jobCreationDuration} ms ` //+
+        //`Days:${(dateTo.getTime()-dateFrom.getTime())/1000 /24/60/60}, `+
+        //`NumOfRows:${numOfRows}`
+    )
+}
+async function runQueries(): Promise<number>  {
+    const dateStart = new Date(2021,11,11)
+    const dateTo7Days = new Date(2021,11,18)
+    const dateTo90Days = new Date(2022,2,10)
+
+    const promises = [
         query(101, dateStart,  dateTo7Days, QueryTypeEnum.global),
         query(102, dateStart,  dateTo90Days, QueryTypeEnum.global),
         query(103, dateStart, dateTo7Days, QueryTypeEnum.top10),
@@ -132,28 +130,16 @@ FROM
         query(105, dateStart, dateTo90Days,QueryTypeEnum.pipeline, 1),
         query(105, dateStart, dateTo90Days,QueryTypeEnum.pipeline, 81),
         query(705, dateStart, dateTo90Days,QueryTypeEnum.pipeline, 101)
-            ]
-        await Promise.all(promises)
-        return 1
-    }
+    ]
+    await Promise.all(promises)
+    return 1
+}
 
-    const dateStart = new Date(2021,11,11)
-    const dateTo7Days = new Date(2021,11,18)
-    const dateTo90Days = new Date(2022,2,10)
-    // [END bigquery_query]
+function main() {
     //runQueries()
     for (let i = 0; i < 5; i++) {
         runQueries()
     }
-    // const promises = [
-    //     runQueries(),
-    //     runQueries(),
-    //     runQueries(),
-    //     runQueries(),
-    //     runQueries()
-    // ]
-
-    //Promise.all(promises)
 }
 
 main();
